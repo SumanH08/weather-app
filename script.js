fba61d5270bd2a9e2a41421c9107f614a870f26f
Suman Hiremath <suman.hiremath89@gmail.com>
2017-08-11 00:00:22 +0530

    integrated ajax request

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ function onload() {
 function readCity() {
   var city = document.getElementById('city').value;
   console.log(city);
+
+  $.ajax({
+    url: "https://instantweatherbot.herokuapp.com/location-query?q="+city
+  }).done(function(res) {
+    console.log(res);
+    var weatherObj = JSON.parse(res);
+    console.log(weatherObj);
+    var location = weatherObj.location;
+    $('#location').html(location);
+
+    var prediction = weatherObj.weather.hourly.summary;
+    console.log(prediction);
+    $("#prediction-text").html(prediction);
+  })
+
 }
 
 function checkDisableGet() {
